b0bc7b647764b464fe62b305ad8d5049be789637
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-07-14 07:55:04 -0400

    re-added colors model

--- db/migrate/20220714115442_create_colors.rb
+++ db/migrate/20220714115442_create_colors.rb
@@ -2,10 +2,10 @@ class CreateColors < ActiveRecord::Migration[7.0]
   def change
     create_table :colors do |t|
       t.string :name
-      t.string :type
+      t.string :elemental
       t.integer :hp
-      t.integer :attack
-      t.integer :defense
+      t.integer :atk
+      t.integer :def
       t.integer :speed
 
       t.timestamps
