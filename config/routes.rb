30cbc3a6d47b5075efff25c8ac2b9b3590043fbc
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-07-14 08:17:34 -0400

    set up color view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "interface#index"
   
   get 'interface/index'
+
+  get "/color", to: "color#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
